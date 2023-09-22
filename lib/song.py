6f9fa1ed075b2df98db094bb1d30b6d75a594436
Iqra Ali <iqra.ali@student.moringaschool.com>
2023-09-22 23:36:46 +0300

    cls-attr

--- lib/song.py
+++ lib/song.py
@@ -3,55 +3,60 @@ class Song:
     genres = []
     artists = []
     genre_count = {}
-    artists_count = {}
+    artist_count = {}
     
     def __init__(self, name, artist, genre):
         self.name = name
         self.artist = artist
         self.genre = genre
-    
+    #incrementing the count cls attr
         Song.count += 1
     
+    #appending the genre and artist to cls if not included
         if genre not in Song.genres:
             Song.genres.append(genre)
             
         if artist not in Song.artists:
             Song.artists.append(artist)
             
+    #updating genre-count dict
         if genre in Song.genre_count:
             Song.genre_count[genre] += 1
         else:
             Song.genre_count[genre] = 1
+            
+    #updating artist-count dict
+        if artist in Song.artist_count:
+            Song.artist_count[artist] += 1
+        else:
+            Song.artist_count[artist] = 1
         
 @classmethod
 def add_to_genres(cls):
-    unique_genres = set(Song.genres)
-    Song.genres = list(unique_genres)
+    #creating a set of unique genres && assgn it to clas attr
+    unique_genres = set(cls.genres)
+    cls.genres = list(unique_genres)
     
 @classmethod
 def add_to_artists(cls):
-    unique_artists = set(Song.artists)
-    Song.artists = list(unique_artists)
+    #creating a set of unique artists && assgn it to cls attr
+    unique_artists = set(cls.artists)
+    cls.artists = list(unique_artists)
     
 @classmethod
 def add_to_genre_count(cls):
     genre_count = {}
-    for song in Song.genre_count:
-        if song in genre_count:
-           genre_count[song] += 1
-        else:
-           genre_count[song] = 1
-    Song.genre_count = genre_count
+    for song in cls.genre_count:
+        genre_count[song] = cls.genre_count[song]
+    cls.genre_count = genre_count#update the genre-count dict val(nw)
 
 @classmethod
 def add_to_artist_count(cls):
     artist_count = {}
-    for artist in Song.artist_count:
-        if artist in artist_count:
-           artist_count[artist] += 1
-        else:
-           artist_count[artist] = 1
-    Song.artist_count = artist_count
-
+    for artist in cls.artist_count:
+        artist_count[artist] = cls.artist_count[artist]
+    cls.artist_count = artist_count#update artist-count dict val(nw)
+    
+#creating song obj
 song = Song("99 Problems", "Jay Z", "Rap")
 pass
