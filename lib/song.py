1537f88e8a26d24422e51bb5c4db955b622d7104
Iqra Ali <iqra.ali@student.moringaschool.com>
2023-09-28 03:31:26 +0300

    cls-attr-n-mthd

--- lib/song.py
+++ lib/song.py
@@ -9,54 +9,51 @@ class Song:
         self.name = name
         self.artist = artist
         self.genre = genre
-    #incrementing the count cls attr
+        # Incrementing the count cls attr
         Song.count += 1
-    
-    #appending the genre and artist to cls if not included
+        
+        # Appending the genre and artist to cls lists if not included
         if genre not in Song.genres:
             Song.genres.append(genre)
-            
         if artist not in Song.artists:
             Song.artists.append(artist)
-            
-    #updating genre-count dict
+        
+        # Updating the genre-count dict
         if genre in Song.genre_count:
             Song.genre_count[genre] += 1
         else:
             Song.genre_count[genre] = 1
-            
-    #updating artist-count dict
+        
+        # Updating the artist-count dict
         if artist in Song.artist_count:
             Song.artist_count[artist] += 1
         else:
             Song.artist_count[artist] = 1
-        
-@classmethod
-def add_to_genres(cls):
-    #creating a set of unique genres && assgn it to clas attr
-    unique_genres = set(cls.genres)
-    cls.genres = list(unique_genres)
-    
-@classmethod
-def add_to_artists(cls):
-    #creating a set of unique artists && assgn it to cls attr
-    unique_artists = set(cls.artists)
-    cls.artists = list(unique_artists)
-    
-@classmethod
-def add_to_genre_count(cls):
-    genre_count = {}
-    for song in cls.genre_count:
-        genre_count[song] = cls.genre_count[song]
-    cls.genre_count = genre_count#update the genre-count dict val(nw)
 
-@classmethod
-def add_to_artist_count(cls):
-    artist_count = {}
-    for artist in cls.artist_count:
-        artist_count[artist] = cls.artist_count[artist]
-    cls.artist_count = artist_count#update artist-count dict val(nw)
-    
-#creating song obj
-song = Song("99 Problems", "Jay Z", "Rap")
-pass
+    @classmethod
+    def add_to_genres(cls):
+        # Creating a set of unique genres and assigning it to the cls attr
+        unique_genres = set(cls.genres)
+        cls.genres = list(unique_genres)
+
+    @classmethod
+    def add_to_artists(cls):
+        # Creating a set of unique artists and assigning it to the cls attr
+        unique_artists = set(cls.artists)
+        cls.artists = list(unique_artists)
+
+    @classmethod
+    def add_to_genre_count(cls):
+        # Creating a new genre count dict and updating the cls attr
+        genre_count = {}
+        for genre in cls.genres:
+            genre_count[genre] = cls.genre_count.get(genre, 0)
+        cls.genre_count = genre_count
+
+    @classmethod
+    def add_to_artist_count(cls):
+        # Creating a new artist count dictand updating the cls attr
+        artist_count = {}
+        for artist in cls.artists:
+            artist_count[artist] = cls.artist_count.get(artist, 0)
+        cls.artist_count = artist_count
